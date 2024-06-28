02836848538e048dc546250672531c50550f3f50
Kassicus <kason.suchow@gmail.com>
2024-06-27 19:34:29 -0600

    The defaults

---
+++ main.py
@@ -0,0 +1,53 @@
+import pygame
+import debug
+
+from settings import *
+
+pygame.init()
+
+class Game():
+    def __init__(self) -> None:
+        self.screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+        pygame.display.set_caption(SCREEN_TITLE)
+
+        self.running = True
+        self.clock = pygame.time.Clock()
+        events = pygame.event.get()
+
+        self.debug_interface = debug.DebugInterface()
+
+    def start(self) -> None:
+        while self.running:
+            self.event_loop()
+            self.draw()
+            self.update()
+
+    def event_loop(self) -> None:
+        events = pygame.event.get()
+
+        for event in events:
+            if event.type == pygame.QUIT:
+                self.running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    self.running = False
+                if event.key == pygame.K_TAB:
+                    self.debug_interface.toggle_active()
+
+    def draw(self) -> None:
+        self.screen.fill(color.black)
+
+        if self.debug_interface.active:
+            self.debug_interface.draw()
+
+    def update(self) -> None:
+
+        self.debug_interface.update(self.clock)            
+        pygame.display.update()
+        delta_time = self.clock.tick(fps_limit) / 1000
+
+if __name__ == '__main__':
+    game = Game()
+    game.start()
+    pygame.quit()
---
+++ settings.py
@@ -0,0 +1,39 @@
+import pygame
+import random
+
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+SCREEN_TITLE = "Color Suvivor"
+
+class ColorLibrary():
+    def __init__(self) -> None:
+        self.black = pygame.Color(0, 0, 0)
+        self.white = pygame.Color(255, 255, 255)
+        self.red = pygame.Color(255, 0, 0)
+        self.green = pygame.Color(0, 255, 0)
+        self.blue = pygame.Color(0, 0, 255)
+
+    def random(self) -> pygame.Color:
+        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        return color
+    
+    def random_custom(self, channels: str) -> pygame.Color:
+        red = 0
+        green = 0
+        blue = 0
+
+        if 'r' in channels:
+            red = random.randint(0, 255)
+        if 'g' in channels:
+            green = random.randint(0, 255)
+        if 'b' in channels:
+            blue = random.randint(0, 255)
+
+        color = pygame.Color(red, green, blue)
+
+        return color
+
+color = ColorLibrary()
+events = None
+delta_time = 0
+fps_limit = 120
